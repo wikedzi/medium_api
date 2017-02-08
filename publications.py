ecc47ac49366edff52be8f3ad42e172fd8f2870c
Timothy <twikedzi@gmail.com>
2017-02-09 02:40:52 +0300

    I have added a fetch function. This function is responsible for connecting with the API, fetching the data and returning a data object that can further be processed

--- publications.py
+++ publications.py
@@ -1,2 +1,12 @@
 import http.client
 import json
+
+def fetchData():
+	#API variables
+	host 	="api.medium.com"
+	auth 	="Bearer 25701d75b893f2e267ea7b8b3af98aa9dd39b190ac738aab0818d7443b2050ebe"
+	cType 	="application/json"
+	accept 	="application/json"
+	userId	="1573b2156fb06a3ed9c6d9eed8f7e24042733be2bfa833bf1af1b30f46ba8a9a7"
+
+
