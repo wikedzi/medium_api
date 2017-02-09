dbdb74f15687daab869f66e23059a55579656ab4
Timothy <twikedzi@gmail.com>
2017-02-09 03:06:10 +0300

    This update inludes Final functionality of the app, where i have added codes to use the FetchData function and work aorund the returned data object. The code also iplements printing functionality where the returned data objected is formatted and printed out to the console. Each publication data is printed out in the format of its ID, its name, its URL and Desription

--- publications.py
+++ publications.py
@@ -9,4 +9,28 @@ def fetchData():
 	accept 	="application/json"
 	userId	="1573b2156fb06a3ed9c6d9eed8f7e24042733be2bfa833bf1af1b30f46ba8a9a7"
 
+	httpHeaders = {"Content-type":cType,"Accept": accept,"Accept-Charset": "utf-8", "Authorization":auth}
+	URI = "/v1/users/"+userId+"/publications"
+
+	#Connecting to Medium with an API using GET method
+	conn = http.client.HTTPSConnection(host)
+	conn.request("GET",URI,"",httpHeaders)
+
+	#Fetching Response object
+	response = conn.getresponse()
+	print(response.status, response.reason)
+	data = response.read()
+	#closing The connection to relase resources
+	conn.close()
+
+	return json.loads(data)# returns a json object of all publications that I own or follows
+
+publications = fetchData()
+for publication in publications['data']:
+	#print(publication)
+    print('ID: {0[id]:s};Name: {0[name]:s};url: {0[url]:s}'.format(publication))
+    print("----------------------------------------------------------------------")
+    print(publication['description'])
+    print("************************************************************************************")
+    print()
 
